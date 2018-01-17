025d68acce3cdfef5d974a95d4aada37b5d49bc2
Derrick Ho <derrickho@derricks-macbook-pro.local>
2018-01-16 16:46:32 -0800

    Initial Commit

---
+++ AutoLayout_SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout_SampleProject
+//
+//  Created by Derrick Ho on 1/16/18.
+//  Copyright © 2018 Derrick Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
